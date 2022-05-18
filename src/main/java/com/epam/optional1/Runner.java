695a90406ea3b17c4bb5f385ee96672c4251d7fe
GalymQA <galymakishevkz@gmail.com>
2022-05-18 17:03:38 +0600

    Optional task #1 done

---
+++ src/main/java/com/epam/optional1/Runner.java
@@ -0,0 +1,82 @@
+package com.epam.optional1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class Runner {
+
+    private static final String FILE_NAME =
+            "/home/titan/Desktop/Epam/Solutions/IOSolutions/io-task/file_with_integers.txt";
+    private static final int NUMBER_OF_INTEGERS_TO_WRITE = 100;
+
+    public static void main(String[] args) {
+        File file = createFile();
+        writeRandomIntegersToFile(file);
+        List<String> integersAsStrings = readIntegersFromFile(file);
+        List<Integer> integers = convertStringsToIntegers(integersAsStrings);
+        Collections.sort(integers);
+        writeSortedIntegersToFile(file, integers);
+    }
+
+    private static File createFile() {
+        File file = new File(FILE_NAME);
+        try {
+            boolean createdStatus = file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    private static void writeRandomIntegersToFile(File file) {
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            Random rand = new Random();
+            for (int i = 0; i < NUMBER_OF_INTEGERS_TO_WRITE; i++) {
+                fileWriter.write(String.valueOf(rand.nextInt()));
+                fileWriter.write('\n');
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String> readIntegersFromFile(File file) {
+        List<String> integersAsStrings = new ArrayList<>();
+        try {
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                integersAsStrings.add(fileReader.nextLine());
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return integersAsStrings;
+    }
+
+    private static List<Integer> convertStringsToIntegers(List<String> integersAsStrings) {
+        List<Integer> integers = new ArrayList<>();
+        for (String integerAsString : integersAsStrings) {
+            integers.add(Integer.parseInt(integerAsString));
+        }
+        return integers;
+    }
+
+    private static void writeSortedIntegersToFile(File file, List<Integer> integers) {
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            for (int integer : integers) {
+                fileWriter.write(String.valueOf(integer));
+                fileWriter.write('\n');
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/epam/optional2/Runner.java
@@ -0,0 +1,10 @@
+package com.epam.optional2;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
